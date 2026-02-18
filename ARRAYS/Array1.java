243d3ff2d388e6201a1540fe3622e76aab15f1b3
Sanskriti Singh <singhsanskriti140804@gmail.com>
2026-02-18 11:08:32 +0530

    Java basic concept practice

---
+++ ARRAYS/Array1.java
@@ -0,0 +1,18 @@
+public class Array1{
+    public static void main(String[] args){
+        int[] arr = new int[5]; //we can also initialize and declare {1,2,3,4,5}
+        //store the data in array 
+        for(int i = 0; i< arr.length; i++){
+            arr[i] = i+1;
+        }
+        //print the stored data
+        for(int i = 0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+        //for each loop to travrse an print data
+        for (int i : arr){
+            System.out.println(i);
+        }
+
+    }
+}
