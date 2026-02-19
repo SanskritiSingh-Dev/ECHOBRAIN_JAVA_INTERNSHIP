2543b938d5389f570e7ed5becd341df48cf47cc2
Sanskriti Singh <singhsanskriti140804@gmail.com>
2026-02-19 14:57:31 +0530

    Insertion Sort practice

--- ARRAYS/InsertionSort.java
+++ ARRAYS/InsertionSort.java
@@ -8,6 +8,21 @@ public class InsertionSort {
             arr[i] = sc.nextInt();
         }
         //insertion sort
-        
+        for(int i = 1; i < arr.length; i++){
+            int curr = arr[i];//current element
+            int prev = i-1;
+            //finding out the position to insert
+            while(prev >= 0 && arr[prev] > curr){
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            arr[prev+1] = curr;
+        }
+        System.out.println("Sorted array is: ");
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+        sc.close();
+
     }
 }
