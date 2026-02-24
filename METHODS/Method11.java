6cfbae770ab0ae3ce2d2a6be26c86fe8b6964069
Sanskriti Singh <singhsanskriti140804@gmail.com>
2026-02-24 12:29:40 +0530

    call by value and call by reference

--- METHODS/Method11.java
+++ METHODS/Method11.java
@@ -10,15 +10,17 @@ public class Method11 {
         i[0] = 100;
     }
     public static void main(String[] args) {
-        //int i = 10;
+        //call by value
+        int i = 10;
+        m1(i);
+        System.out.println(i);
+
+        //call by ref
         int[] arr = new int[]{1,2,3};
-        //m1(i);
         for (int j : arr) {
             System.out.println(j+ " ");
         }
-        m2(arr); //calling the method 
-        //System.out.println(i);
-        
+        m2(arr); //calling the method     
         System.out.println();
         for (int k : arr) {
             System.out.println(k+ " ");
