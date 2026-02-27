3ff7cf7b06907a18b3d53ae5b72a915fcea124a0
Sanskriti Singh <singhsanskriti140804@gmail.com>
2026-02-27 11:31:13 +0530

    Override Constructor

--- CONSTRUCTOR/Employee.java
+++ CONSTRUCTOR/Employee.java
@@ -4,26 +4,34 @@ public class Employee {
     int empId;
     String job;
     double salary;
+    long phno; //option for user
+
+    //override constructor - we want this data mandetory.
+    public Employee(String name, int empId){
+        this.name = name;
+        this.empId = empId;
+    }
 
     //default constructor
     public Employee(){
 
     }
-    //local var as parameter in this constructor
-    Employee(String name, int empId, String job, double salary){
+    //local var as parameter in this constructor main const with all parameter
+    Employee(String name, int empId, String job, double salary, long phno){
         this.name = name;
         this.empId = empId;
         this.job = job;
         this.salary = salary;
+        this.phno = phno; // optional
     }
     //method to display
     public void display(){
-        System.out.println("Name: " + name);
         System.out.println("empId: "+ empId);
+        System.out.println("Name: " + name);
         System.out.println("Job: "+ job);
         System.out.println("Samalry: "+ salary);
     }
 
-    
+
     
 }
---
+++ CONSTRUCTOR/EmployeeMain.java
@@ -0,0 +1,17 @@
+public class EmployeeMain {
+    public static void main(String[] args) {
+        //employee object is created
+        //it will check if any employee file is there in the current directoy.
+        //no error because we have created the default constructor.
+
+        Employee emp = new Employee("Jibili", 1001, "SDE", 2000000, 8899551123l);
+        emp.display();
+
+        System.out.println();
+        
+        //user dont want to enter the phno so for this we have other const.
+        Employee emp2 = new Employee("Zimbha", 1002); 
+        emp2.display();
+    }
+    
+}
