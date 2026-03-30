da8065289be1523854d335ff8912fbd10e568182
Sanskriti Singh <singhsanskriti140804@gmail.com>
2026-03-31 01:38:26 +0530

    Writing with array Buffered Output Stream (output stream)

---
+++ STREAMS/BufferedOutputExample.java
@@ -0,0 +1,39 @@
+package STREAMS;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class BufferedOutputExample {
+
+    public static void main(String[] args) {
+
+        try {
+            // Create FileOutputStream
+            FileOutputStream fos = new FileOutputStream("buffered.txt");
+
+            // Wrap it with BufferedOutputStream
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+            // Data to write
+            String data = "Hello Buffered Byte Stream using Array!";
+
+            // Convert string to byte array
+            byte[] bytes = data.getBytes();
+
+            // Write byte array into buffer
+            bos.write(bytes);
+
+            // Flush ensures all data is written to file
+            bos.flush();
+
+            // Close stream
+            bos.close();
+
+            System.out.println("Data written using BufferedOutputStream!");
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
