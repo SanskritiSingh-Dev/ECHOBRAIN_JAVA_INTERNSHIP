77ebbb76108324b8b6143d10768282dda98aef8d
Sanskriti Singh <singhsanskriti140804@gmail.com>
2026-03-31 12:16:33 +0530

    InputStreamReader, convert byte to char

---
+++ STREAMS/InputStreamReaderExample.java
@@ -0,0 +1,27 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class InputStreamReaderExample {
+
+    public static void main(String[] args) {
+
+        try {
+            // Input from keyboard (System.in is byte stream)
+            InputStreamReader isr = new InputStreamReader(System.in);
+
+            int data;
+
+            System.out.println("Enter characters (press Enter to stop):");
+
+            // Read characters from keyboard
+            while ((data = isr.read()) != '\n') {
+                System.out.print((char) data);
+            }
+
+            isr.close();
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
