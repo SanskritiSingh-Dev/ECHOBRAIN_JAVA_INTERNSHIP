582e035f8cb172c4f2ee2f88194b4c527d67bbb8
Sanskriti Singh <singhsanskriti140804@gmail.com>
2026-03-31 12:05:04 +0530

    File reading using FileReader in character Stream

---
+++ STREAMS/CharacterReadExample.java
@@ -0,0 +1,23 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CharacterReadExample {
+
+    public static void main(String[] args) {
+        // Open file for reading
+        try(FileReader fr = new FileReader("char.txt")) {
+            
+            int data;
+
+            // Read character by character
+            while ((data = fr.read()) != -1) {
+
+                // Convert int to char
+                System.out.print((char) data);
+            }
+            
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
