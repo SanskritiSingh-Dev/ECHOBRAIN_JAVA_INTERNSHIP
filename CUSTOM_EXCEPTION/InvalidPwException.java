e31a425d6c0d86db232dd177d962d7d88d3b5d74
Sanskriti Singh <singhsanskriti140804@gmail.com>
2026-04-01 00:07:08 +0530

    Invalid password exception

---
+++ CUSTOM_EXCEPTION/InvalidPwException.java
@@ -0,0 +1,31 @@
+package CUSTOM_EXCEPTION;
+/*Invalid Password Exception
+Createa custom exception InvalidPasswordException and take password from
+scanner
+ Password must be at least 8 characters
+ Throw exception if invalid */
+
+import java.util.Scanner;
+
+class InvalidPasswordException extends Exception {
+    public InvalidPasswordException(String message) {
+        super(message);
+    }
+}
+public class InvalidPwException {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("Enter your password:");
+            String password = sc.nextLine();
+            if(password.length() < 8) {
+                throw new InvalidPasswordException("Password must be at least 8 characters long.");
+            } else {
+                System.out.println("Password is valid.");
+            }
+        } catch (InvalidPasswordException e) {
+            System.out.println("Caught Exception: " + e.getMessage());
+        }
+    }
+    
+}
